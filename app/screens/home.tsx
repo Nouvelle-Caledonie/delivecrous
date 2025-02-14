84f3aa1f064640dc87b5a26095d7db5a527b60d4
joran-cng <caunegre.joran@gmail.com>
2025-02-14 14:40:47 +0100

    Add Header & home page & product page

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Header from '../components/header';
+import ItemCard from '../components/itemCard';
+import { api } from '../services/api';
 
-export default function Home() {
-    console.log("HomeScreen chargé !");
+const HomeScreen: React.FC = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await api.getPlats();
+                setProducts(response);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des plats:', error);
+            }
+        };
+        fetchProducts();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Bienvenue sur la page d'accueil !</Text>
+            <Text style={styles.title}>La carte</Text>
+            <FlatList
+                data={products}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => <ItemCard product={item} />}
+            />
         </View>
     );
 };
@@ -15,11 +36,13 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        padding: 20,
     },
-    text: {
-        fontSize: 18,
+    title: {
+        fontSize: 24,
         fontWeight: 'bold',
-        marginTop: 20,
+        marginVertical: 20,
     },
 });
+
+export default HomeScreen;
